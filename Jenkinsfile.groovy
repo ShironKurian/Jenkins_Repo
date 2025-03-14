2d99dfce82d2ea171d197d2ca359d5b6f7d8398a
ShironKurian <62640580+ShironKurian@users.noreply.github.com>
2025-03-14 19:44:28 -0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git branch: 'main', url: 'https://github.com/your-username/your-repo.git'
+                git branch: 'main', url: 'https://github.com/ShironKurian/Jenkins_Repo.git'
             }
         }
         stage('Build') {
